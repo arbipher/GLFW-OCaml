27ba929ea0f0b9dac5e64e054b31a6ef7dc96c7c
Weng Shiwei <arbipher@gmail.com>
2025-10-27 11:58:55 -0500

    Update example to use recent API.

--- example/window/window.ml
+++ example/window/window.ml
@@ -5,15 +5,17 @@ let () =
   GLFW.init ();
   at_exit GLFW.terminate;
   (* Create a windowed mode window and its OpenGL context *)
-  let window = GLFW.createWindow 640 480 "Hello World" () in
+  let window =
+    GLFW.createWindow ~width:640 ~height:480 ~title:"Hello World" ()
+  in
   (* Make the window's context current *)
-  GLFW.makeContextCurrent (Some window);
+  GLFW.makeContextCurrent ~window:(Some window);
   (* Loop until the user closes the window *)
-  while not (GLFW.windowShouldClose window) do
+  while not (GLFW.windowShouldClose ~window) do
     (* Render here *)
     glClearColorBufferBit ();
     (* Swap front and back buffers *)
-    GLFW.swapBuffers window;
+    GLFW.swapBuffers ~window;
     (* Poll for and process events *)
     GLFW.pollEvents ()
   done
